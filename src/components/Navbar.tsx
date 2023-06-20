e319c5da1b3ecc4001be9d3f01c41a543ebb8467
msatech <deependrakumar049@gmail.com>
2023-06-20 23:13:48 +0530

    schema added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,36 @@
-import { FC } from 'react'
+import Link from "next/link";
+import { FC } from "react";
+import { Icons } from "./Icons";
+import { buttonVariants } from "./ui/Button";
+import { getAuthSession } from "@/lib/auth";
 
-interface NavbarProps {
-  
-}
+interface NavbarProps {}
 
-const Navbar: FC<NavbarProps> = ({}) => {
-  return <div>Navbar</div>
-}
+const Navbar: FC<NavbarProps> = async ({}) => {
+  const session = await getAuthSession();
+  return (
+    <div className="fixed top-0 inset-x-0 h-fit  bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
+      <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
+        {/* Logo */}
+        <Link href="/" className="flex gap-2 items-center">
+          <Icons.logo className="h-8 w-8 " />
+          <p className="hidden text-zinc-700 text-lg font-bold md:block">
+            DEV COM
+          </p>
+        </Link>
 
-export default Navbar
+        {/* Seach bar */}
+
+        {session ? (
+          <p>youre logged in </p>
+        ) : (
+          <Link href="/sign-in" className={buttonVariants()}>
+            Sign In
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
